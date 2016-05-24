d19ca95004be78800b6bad4dd3e2a8fc4f421938
Jason McKay <jasonwilliammckay@gmail.com>
2016-05-24 18:22:21 -0500

    Changed loops to python ranges. Added input validation check. Removed a few needless variables that weren't doing anything.

--- AdventOfCode_10/AdventOfCode_10.py
+++ AdventOfCode_10/AdventOfCode_10.py
@@ -1,42 +1,36 @@
 # Per http://adventofcode.com/day/10
 
+iterations = 40
 startingInput = "1321131112"
-iterations = 50
 
 def main():
     input = startingInput
-    i = 0
+    
+    if (len(startingInput) > 0):
+        for i in range(0, iterations):
+            input = seeAndSay(input)
 
-    while (i < iterations):
-        input = seeAndSay(input)
-        i += 1
-
-    print "For the input {0}, across {1} iterations, the length of the result is {2:,}.".format(startingInput, iterations, len(input))
+    print "For the input \"{0}\", across {1} iterations, the length of the result is {2:,}.".format(startingInput, iterations, len(input))
 
 def seeAndSay(input):
 
-    fullString = ""
     charName = input[0]
     charLen = 1
-    i = 1;
-    x = [];
+    resultList = [];
 
-    while (i < len(input)):
+    for i in range (1, len(input)):
 
         if (input[i] == charName):
             charLen += 1
 
         else:
-            x.append(str(charLen))
-            x.append(str(charName))
+            resultList.append(str(charLen))
+            resultList.append(str(charName))
             charName = input[i]
             charLen = 1
-
-        i += 1
     
-    x.append(str(charLen))
-    x.append(str(charName))
-    fullString = "".join(x)
-    return fullString
+    resultList.append(str(charLen))
+    resultList.append(str(charName))
+    return "".join(resultList)
     
 main()
