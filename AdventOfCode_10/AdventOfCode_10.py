679db13e563082f1f8661db52bfd3d057e17731e
Jason McKay <jasonwilliammckay@gmail.com>
2016-05-24 20:51:51 -0500

    Broke repeated code into a new function (addToList) Added a few comments to clarify code reasoning.

--- AdventOfCode_10/AdventOfCode_10.py
+++ AdventOfCode_10/AdventOfCode_10.py
@@ -6,31 +6,31 @@ startingInput = "1321131112"
 def main():
     input = startingInput
     
-    if (len(startingInput) > 0):
+    if (len(startingInput) > 0):        # check for valid input
         for i in range(0, iterations):
             input = seeAndSay(input)
 
     print "For the input \"{0}\", across {1} iterations, the length of the result is {2:,}.".format(startingInput, iterations, len(input))
 
-def seeAndSay(input):
+def addToList(list, item1, item2):
+    list.append(str(item1))
+    list.append(str(item2))
+    return list
 
-    charName = input[0]
+def seeAndSay(input):
+    charName = input[0]         # pre-load the first character in the string to be checked    
     charLen = 1
     resultList = [];
 
     for i in range (1, len(input)):
-
         if (input[i] == charName):
             charLen += 1
-
         else:
-            resultList.append(str(charLen))
-            resultList.append(str(charName))
+            resultList = addToList(resultList, charLen, charName)
             charName = input[i]
             charLen = 1
-    
-    resultList.append(str(charLen))
-    resultList.append(str(charName))
-    return "".join(resultList)
+
+    resultList = addToList(resultList, charLen, charName)   # add the final substring to the list
+    return ''.join(resultList)
     
 main()
